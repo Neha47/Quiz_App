14f809fab614f67e26303f24ca05f913152a2fee
Neha47 <nehabhatt7575@gmail.com>
2023-08-05 00:25:53 +0530

    A simple Quiz App.

---
+++ app/src/main/java/com/example/quiz_app/QuestionAnswer.java
@@ -0,0 +1,31 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+
+    public static String question[] ={
+            "What is the full form of “AI”?",
+            "Who is the inventor of Artificial Intelligence?",
+            "Which of the following laws is associated with the heating effect of electric current ?",
+            "Which element in the periodic table forms maximum number of compounds ?",
+            "A body at rest can have ?"
+
+    };
+
+    public static String choices[][] = {
+            {"Artificially Intelligent","Artificial Intelligence","Artificially Intelligence","Advanced Intelligence"},
+            {"Geoffrey Hinton","Andrew Ng","John McCarthy","Jürgen Schmidhuber"},
+            {"Joule's law","Ohm's law","Faraday's law","Newton's law"},
+            {"H","O","S","C"},
+            {"Speed","Velocity","Momentum","Energy"}
+    };
+
+    public static String correctAnswers[] = {
+            "Artificial Intelligence",
+            "John McCarthy",
+            "Joule's law",
+            "C",
+            "Velocity"
+    };
+
+}
